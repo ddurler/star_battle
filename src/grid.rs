68703cc49f10d6450140aaa4af3d34b3b614dc79
DDurler <denis.durler@gmail.com>
2024-08-18 18:50:34 +0200

    feat: Add Grid

---
+++ src/grid.rs
@@ -0,0 +1,105 @@
+//! Contenu des case de la grille.
+
+use crate::CellValue;
+use crate::GridCell;
+use crate::GridHandler;
+use crate::LineColumn;
+
+/// Cases de la grille
+#[derive(Clone, Debug, Default, PartialEq, Eq, Hash)]
+pub struct Grid {
+    /// Dimensions de la grille
+    size: LineColumn,
+
+    /// Cases de la grille
+    cells: Vec<Vec<GridCell>>,
+}
+
+impl From<&GridHandler> for Grid {
+    fn from(value: &GridHandler) -> Self {
+        let nb_lines = value.nb_lines();
+        let nb_columns = value.nb_columns();
+        let mut cells = Vec::with_capacity(nb_lines);
+        for line in 0..nb_lines {
+            let mut cells_line = Vec::with_capacity(nb_columns);
+            for column in 0..nb_columns {
+                let line_column = LineColumn::new(line, column);
+                let grid_cell = GridCell {
+                    line_column,
+                    region: value.cell_region(&line_column),
+                    value: CellValue::Unknown,
+                };
+                cells_line.push(grid_cell);
+            }
+            cells.push(cells_line);
+        }
+        Self {
+            size: LineColumn::new(nb_lines, nb_columns),
+            cells,
+        }
+    }
+}
+
+impl Grid {
+    /// Nombre de lignes de la grille
+    #[must_use]
+    pub const fn nb_lines(&self) -> usize {
+        self.size.line
+    }
+
+    /// Nombre de colonnes de la grille
+    #[must_use]
+    pub const fn nb_columns(&self) -> usize {
+        self.size.column
+    }
+
+    /// Retourne la case (non mutable) de la grille en (line, column)
+    #[must_use]
+    pub fn cell(&self, line_column: &LineColumn) -> &GridCell {
+        &self.cells[line_column.line][line_column.column]
+    }
+
+    /// Retourne la case (mutable) de la grille en (line, column)
+    #[must_use]
+    pub fn cell_mut(&mut self, line_column: &LineColumn) -> &mut GridCell {
+        &mut self.cells[line_column.line][line_column.column]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::GridParser;
+
+    #[test]
+    fn test_from_grid_handler() {
+        let parser =
+            GridParser::try_from(vec!["ABBBB", "ABBBB", "CCBBB", "DDDDD", "DEEED"]).unwrap();
+        let handler = GridHandler::new(&parser, 1);
+        let grid = Grid::from(&handler);
+
+        assert_eq!(grid.nb_lines(), 5);
+        assert_eq!(grid.nb_columns(), 5);
+
+        for line in 0..grid.nb_lines() {
+            for column in 0..grid.nb_columns() {
+                let line_column = LineColumn::new(line, column);
+                assert_eq!(grid.cell(&line_column).value, CellValue::Unknown);
+            }
+        }
+    }
+
+    #[test]
+    fn test_clone_cell_mut() {
+        let parser =
+            GridParser::try_from(vec!["ABBBB", "ABBBB", "CCBBB", "DDDDD", "DEEED"]).unwrap();
+        let handler = GridHandler::new(&parser, 1);
+        let grid = Grid::from(&handler);
+
+        let mut grid_cloned = grid.clone();
+        let line_column = LineColumn::new(0, 0);
+        grid_cloned.cell_mut(&line_column).value = CellValue::Star;
+        assert_eq!(grid.cell(&line_column).value, CellValue::Unknown);
+        assert_eq!(grid_cloned.cell(&line_column).value, CellValue::Star);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -41,6 +41,7 @@ Les lignes 'vides' ou qui débutent par l'un des caractères suivants sont ignor
 
 ```rust
 use star_battle::GridParser;
+
 assert!(GridParser::try_from("
     ABBBB
     ABBBB
@@ -50,15 +51,23 @@ assert!(GridParser::try_from("
 ").is_ok());
 ```
 
+Le [`GridParser`] est utilisé pour définir la grille initiale. La cohérence de la grille est vérifiée:
+
+* Syntaxe correcte dans le texte descriptif de la grille
+* Nombre cohérent de colonne dans chaque ligne
+* Régions connexes dans la grille
+
 ## [`LineColumn`]
 
 [`LineColumn`] repère une case dans la grille par ses coordonnées (`line`, `column`) base 0.
 
 ```rust
 use star_battle::LineColumn;
-let lc = LineColumn::new(0, 0);
-assert_eq!(lc.line(), 0);
-assert_eq!(lc.column(), 0);
+
+let line_column = LineColumn::new(0, 1);
+
+assert_eq!(line_column.line(), 0);
+assert_eq!(line_column.column(), 1);
 ```
 
 ## [`CellValue`]
@@ -74,9 +83,9 @@ use star_battle::CellValue;
 assert_eq!(CellValue::default(), CellValue::Unknown);
 ```
 
-## [`CellValue`]
+## [`GridCell`]
 
-[`CellValue`] décrit une case de la grille parsée par [`GridParser`]:
+[`GridCell`] décrit une case de la grille parsée par [`GridParser`] ou gérée par [`Grid`]:
 
 * `line_column`: [`LineColumn`] de la case dans la grille (base 0)
 * `region`: [`Region`] de la case
@@ -84,13 +93,9 @@ assert_eq!(CellValue::default(), CellValue::Unknown);
 
 ```rust
 use star_battle::{GridParser, LineColumn, CellValue};
-let parser = GridParser::try_from("
-    ABBBB
-    ABBBB
-    CCBBB
-    DDDDD
-    DEEED
-").unwrap();
+
+let parser = GridParser::try_from(vec!["ABBBB", "ABBBB", "CCBBB", "DDDDD", "DEEED"]).unwrap();
+
 assert_eq!(parser.cell(&LineColumn::new(0, 0)).unwrap().region, 'A');
 assert_eq!(parser.cell(&LineColumn::new(0, 0)).unwrap().value, CellValue::Unknown);
 ```
@@ -105,23 +110,60 @@ assert_eq!(parser.cell(&LineColumn::new(0, 0)).unwrap().value, CellValue::Unknow
 * `regions`: liste des régions de la grille (par ordre alphabétique)
 * `cell_region`: région d'une case de la grille
 
-Les contenus des cases de la grille ne sont pas définis dans la structure [`GridHandler`].
+Les contenus des cases de la grille ne sont pas définis dans la structure [`GridHandler`].<br>
+C'est la structure [`Grid`] qui représente le contenu des cases de la grille.
+
+Initialement, le [`GridHandler`] est construite à partir d'un [`GridParser`].
 
 ```rust
 use star_battle::{GridParser, GridHandler, LineColumn};
-let parser = GridParser::try_from("
-    ABBBB
-    ABBBB
-    CCBBB
-    DDDDD
-    DEEED
-").unwrap();
+
+let parser = GridParser::try_from(vec!["ABBBB", "ABBBB", "CCBBB", "DDDDD", "DEEED"]).unwrap();
 let grid = GridHandler::new(&parser, 1);
+
 assert_eq!(grid.nb_lines(), 5);
 assert_eq!(grid.nb_columns(), 5);
 assert_eq!(grid.nb_stars(), 1);
 assert_eq!(grid.regions(), vec!['A', 'B', 'C', 'D', 'E']);
 assert_eq!(grid.cell_region(&LineColumn::new(0, 0)), 'A');
+```
+
+## [`Grid`]
+
+[`Grid`] est la structure avec le contenu des cases de la grille.
+
+Cette structure est utilisée pour la resolution du jeu. Elle est allégée des informations détenues par la
+structure associée [`GridHandler`]; Ce qui permet d'examiner des évolutions de la grille avec un minimum
+d'occupation de mémoire.
+
+Initialement, la [`Grid`] est construite à partir d'un [`GridHandler`].
+
+```rust
+use star_battle::{GridParser, GridHandler, Grid};
+
+let parser = GridParser::try_from(vec!["ABBBB", "ABBBB", "CCBBB", "DDDDD", "DEEED"]).unwrap();
+let grid_handler = GridHandler::new(&parser, 1);
+let grid = Grid::from(&grid_handler);
+
+assert_eq!(grid.nb_lines(), 5);
+assert_eq!(grid.nb_columns(), 5);
+```
+
+On peut ainsi utiliser la structure [`Grid`] pour résoudre le jeu en clonant cette structure et en
+postulant sur la valeur des cases de la grille pour évaluer les possibilités.
+
+```rust
+use star_battle::{GridParser, GridHandler, Grid, LineColumn, CellValue};
+
+let parser = GridParser::try_from(vec!["ABBBB", "ABBBB", "CCBBB", "DDDDD", "DEEED"]).unwrap();
+let grid_handler = GridHandler::new(&parser, 1);
+let grid = Grid::from(&grid_handler);
+
+let mut grid_cloned = grid.clone();
+let line_column = LineColumn::new(0, 0);
+grid_cloned.cell_mut(&line_column).value = CellValue::Star;
+assert_eq!(grid.cell(&line_column).value, CellValue::Unknown);
+assert_eq!(grid_cloned.cell(&line_column).value, CellValue::Star);
 ```
 
 */
@@ -131,6 +173,7 @@ pub type Region = char;
 
 // Modules
 mod cell_value;
+mod grid;
 mod grid_cell;
 mod grid_handler;
 mod grid_parser;
@@ -142,6 +185,7 @@ use grid_parser_checker::GridParserChecker;
 
 // Exported
 pub use cell_value::CellValue;
+pub use grid::Grid;
 pub use grid_cell::GridCell;
 pub use grid_handler::GridHandler;
 pub use grid_parser::GridParser;
